b5a1669cb052507c6340550d289793b6fd7f2d25
Nazarii Moshenskyi <nazarii.moshenskyi@globallogic.com>
2019-05-11 19:49:26 +0300

    Added frame

--- app/src/main/java/com/moshenskyi/bullsheepandroid/MainActivity.java
+++ app/src/main/java/com/moshenskyi/bullsheepandroid/MainActivity.java
@@ -4,10 +4,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.TrackingState;
@@ -16,12 +14,18 @@ import com.google.ar.sceneform.animation.ModelAnimator;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.AnimationData;
+import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.PlaneRenderer;
+import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-import java.util.function.Function;
+import java.util.concurrent.CompletableFuture;
+
+import static com.google.ar.sceneform.rendering.PlaneRenderer.MATERIAL_TEXTURE;
+import static com.google.ar.sceneform.rendering.PlaneRenderer.MATERIAL_UV_SCALE;
 
 public class MainActivity extends AppCompatActivity {
     private ArFragment arFragment;
@@ -55,12 +59,48 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.statistics).setVisibility(View.VISIBLE);
             }
         });
+
         arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
             com.google.ar.core.Camera camera = arFragment.getArSceneView().getArFrame().getCamera();
             if (camera.getTrackingState() == TrackingState.TRACKING) {
                 arFragment.getPlaneDiscoveryController().hide();
                 findSurfaceTv.setText("Tap to place");
             }
+//            setPlaneTexture("frame.png");
+        });
+    }
+
+    private void setupPlaneRenderer() {
+        CompletableFuture<Texture> build = Texture.builder().setSource(this, R.drawable
+                .square_plane_renderer)
+                .build();
+        arFragment.getArSceneView()
+                .getPlaneRenderer()
+                .getMaterial()
+                .thenAcceptBoth(build, (material, texture) -> {
+                    material.setTexture(MATERIAL_TEXTURE, texture);
+                    material.setFloat2(MATERIAL_UV_SCALE, 10.0f, 10.0f);
+                });
+    }
+
+    private void setPlaneTexture(String path) {
+
+        Texture.Sampler sampler = Texture.Sampler.builder()
+                .setMinFilter(Texture.Sampler.MinFilter.NEAREST)
+                .setMagFilter(Texture.Sampler.MagFilter.NEAREST)
+                .setWrapModeR(Texture.Sampler.WrapMode.CLAMP_TO_EDGE)
+                .setWrapModeS(Texture.Sampler.WrapMode.CLAMP_TO_EDGE)
+                .setWrapModeT(Texture.Sampler.WrapMode.CLAMP_TO_EDGE)
+                .build();
+
+        Texture.builder().setSource(() -> getAssets().open(path))
+                .setSampler(sampler)
+                .build().thenAccept((texture) -> {
+            arFragment.getArSceneView().getPlaneRenderer().getMaterial()
+                    .thenAccept((material) -> {
+                        material.setTexture(MATERIAL_TEXTURE, texture);
+                        material.setFloat(MATERIAL_UV_SCALE,1f);
+                    });
         });
     }
 
@@ -130,18 +170,19 @@ public class MainActivity extends AppCompatActivity {
                     // initClickListeners();
 
                     // Adds ability to rotate, move and resize the node.
-                    TransformableNode musicControlView = new TransformableNode(arFragment
+                    TransformableNode childNode = new TransformableNode(arFragment
                             .getTransformationSystem());
                     // Appears above higher than ModelRenderable by 0,8f on y-axis.
-                    musicControlView.setLocalPosition(new Vector3(0f, transformableNode
+                    childNode.setLocalPosition(new Vector3(0f, transformableNode
                             .getLocalPosition().y + 0.8f, 0f));
                     // Attaches this ViewRenderable to anchor node, which contains some coordinates.
                     // So, after setting a local position for a node - we set it relatively to
                     // this anchor node.
-                    musicControlView.setParent(anchorNode);
+                    childNode.setParent(anchorNode);
                     // We set here a renderable for our node. It is a ready to use layout, which was
                     // inflated is setView few lines above.
-                    musicControlView.setRenderable(viewRenderable);
+                    childNode.setRenderable(viewRenderable);
+
                 });
     }
 }
